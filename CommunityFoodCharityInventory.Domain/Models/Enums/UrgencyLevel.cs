2f19299380dc137da4594219424e0bff8411cbe7
Khodani <ludere.khodani@outlook.com>
2026-07-10 23:20:37 +0200

    New: Enum for urgency level

---
+++ CommunityFoodCharityInventory.Domain/Models/Enums/UrgencyLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunityFoodCharityInventory.Domain.Models.Enums
+{
+    public enum UrgencyLevel
+    {
+
+        Stable,
+        LowStock,
+        Critical
+    }
+}
