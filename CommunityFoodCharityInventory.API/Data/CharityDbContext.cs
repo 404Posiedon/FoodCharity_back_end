b97f7ce3a7a4c51f6220bbde0161b74a4743e57c
Khodani <ludere.khodani@outlook.com>
2026-07-11 21:38:44 +0200

    Add: Database context for foodPulse

---
+++ CommunityFoodCharityInventory.API/Data/CharityDbContext.cs
@@ -0,0 +1,32 @@
+﻿using CommunityFoodCharityInventory.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommunityFoodCharityInventory.API.Data
+{
+    public class CharityDbContext:DbContext
+    {
+        //Constructor
+        public CharityDbContext(DbContextOptions<CharityDbContext> options) : base(options) { }
+
+        public DbSet<InventoryItem> FoodInventry => Set<InventoryItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // EF Core mapping setup for the private properties in your Domain Model
+            modelBuilder.Entity<InventoryItem>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired();
+
+                // Native EF Core Optimistic Concurrency Token handling
+                entity.Property(e => e.RowVersion)
+                      .IsRowVersion();
+            });
+
+
+        }
+       
+    }
+}
